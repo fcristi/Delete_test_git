7894100d62bde2a01ae1741cd2f38ebf21fb5e65
Cristian Fota <cristian.fota@btrl.ro>
2018-10-23 11:21:13 +0300

    Add project files.

---
+++ Delete_test_git/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Delete_test_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Git test");
+
+            Console.ReadKey();
+        }
+    }
+}
